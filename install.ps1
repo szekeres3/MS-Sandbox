8986cabf3739a4d4183109d761b72343dba04ef2
Szekeres Ábel <abelsz100@gmail.com>
2025-01-16 13:55:38 +0100

    separate dev tools

--- install.ps1
+++ install.ps1
@@ -3,10 +3,13 @@ Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
 Repair-WinGetPackageManager
 
 winget.exe install -e --id NASM.NASM --accept-package-agreements --accept-source-agreements
-winget.exe install -e --id Git.Git --accept-package-agreements --accept-source-agreements
-winget.exe install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
 winget.exe install -e --id Microsoft.VisualStudio.2022.BuildTools --accept-package-agreements --accept-source-agreements
-winget.exe install -e --id Neovim.Neovim --accept-package-agreements --accept-source-agreements
+
+if ($args.Contains("-d")) {
+    winget.exe install -e --id Git.Git --accept-package-agreements --accept-source-agreements
+    winget.exe install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+    winget.exe install -e --id Neovim.Neovim --accept-package-agreements --accept-source-agreements
+}
 
 & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" `
 modify `
