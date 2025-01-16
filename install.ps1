3d323e0eaba78c5a0cf4ecdeb460bf7e1f1b547c
Szekeres Ábel <abelsz100@gmail.com>
2025-01-16 13:52:27 +0100

    .exe

--- install.ps1
+++ install.ps1
@@ -2,11 +2,11 @@ Install-PackageProvider -Name NuGet -Force
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
 Repair-WinGetPackageManager
 
-winget install -e --id NASM.NASM --accept-package-agreements --accept-source-agreements
-winget install -e --id Git.Git --accept-package-agreements --accept-source-agreements
-winget install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
-winget install -e --id Microsoft.VisualStudio.2022.BuildTools --accept-package-agreements --accept-source-agreements
-winget install -e --id Neovim.Neovim --accept-package-agreements --accept-source-agreements
+winget.exe install -e --id NASM.NASM --accept-package-agreements --accept-source-agreements
+winget.exe install -e --id Git.Git --accept-package-agreements --accept-source-agreements
+winget.exe install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+winget.exe install -e --id Microsoft.VisualStudio.2022.BuildTools --accept-package-agreements --accept-source-agreements
+winget.exe install -e --id Neovim.Neovim --accept-package-agreements --accept-source-agreements
 
 & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" `
 modify `
